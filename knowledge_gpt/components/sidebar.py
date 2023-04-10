3d57b267a867ac98776a200af8cfd05391a5c317
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2023-04-10 11:51:53 +0500

    chore: remove github links

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -32,12 +32,4 @@ def sidebar():
             "📖BookBiteGPT allows you to ask questions about your "
             "documents and get accurate answers with instant citations. "
         )
-        st.markdown(
-            "This tool is a work in progress. "
-            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
-            "with your feedback and suggestions💡"
-        )
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
-        st.markdown("---")
-
         faq()
